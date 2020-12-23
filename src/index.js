3e8911ca7c5eca05e4694f708f9625d738e7f001
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2020-12-23 10:53:19 +0800

    Adding Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App'
 //
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
+const logger = createLogger();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = 
+createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+// const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App/>
+  </Provider>, 
+  document.getElementById('root'));
 //
